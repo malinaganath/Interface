5f3caa27007c898050da51f762dd5efb257bc91c
malinaganath <malinaganath@outlook.com>
2020-07-26 18:42:26 +0530

    First commit

---
+++ Interface/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+   
+    interface IOffer
+    {
+        string GetMyOffer();
+    }
+    interface IAccount : IOffer
+    {
+        int MyProperty { get; set; }
+
+        double GetBalance();
+        bool Save();
+    }
+    class Program : IAccount
+    {
+        public int MyProperty
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public double GetBalance()
+        {
+            return 100;
+        }
+        public bool Save()
+        {
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+        public string GetMyOffer()
+        {
+            return "You Not Have any Offer";
+        }
+
+
+
+    }
+
+    class Test
+    {
+        string a = "I am Naga";
+        bool values;
+
+        void Test1()
+        {
+            GetTempResult();
+        }
+
+        private void GetTempResult()
+        {
+            if (a.Contains("Naga"))
+                values = true;
+        }
+    }
+}
+
